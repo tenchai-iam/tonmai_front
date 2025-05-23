aa575b713b160bf394e458c75fdd15957a89d8fd
DESKTOP-FUERDP8\SIMPITEC_APP <wonggidbird@gmail.com>
2025-05-23 16:57:10 +0700

    add all file

--- config_blank.py
+++
@@ -1,9 +0,0 @@
-# this config.py should only contain the path to where the data is being stored to be used in the corridor model pipeline
-
-BASE_PATH = ""
-DB_SERVER = ""
-DB_PORT = ""
-DB_NAME = 'VEGETATION'
-DB_USERNAME = ""
-DB_PASSWORD = ""
-
