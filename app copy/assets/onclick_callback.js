610d6cd6489d263330aa05a6c425a815ede0a64b
ttboomerang91 <norraphat.tia@pea.co.th>
2025-05-23 14:28:12 +0700

    test

--- app copy/assets/onclick_callback.js
+++
@@ -1,5 +0,0 @@
-//Listen for a message from the map html. If found, send the message to the provided asset ("region selector" or "asset_selector") element's "value property"
-window.onmessage = function(e) {
-        console.log(JSON.stringify(e.data));
-        dash_clientside.set_props(e.data['type'], {'value' : e.data['value']});
-}
