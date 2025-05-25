daf8416ffba7eae62776fc172c919bac59919ad6
tenchai-iam <tenchai@iamthongtong.me>
2025-05-25 17:31:33 +0000

    26.05 Update

--- src/ComponentsPage/Main/Map.js
+++ src/ComponentsPage/Main/Map.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Select from "react-select";
 
 import NavbarComponent from "../Sub/NavbarComponent.js";
 
@@ -9,6 +10,36 @@ const Map = () => {
     <div>
       <NavbarComponent />
       <div className="header-container">แผนการตัดต้นไม้</div>
+      <div className="main-container">
+        <div className="dropdown-dropdown-container">
+          <div className="dropdowngroup-container">
+            <Select
+              placeholder="ค้นหา/เลือกการไฟฟ้าเขต"
+              noOptionsMessage={() => "ไม่พบข้อมูล"}
+              className="react-select-container"
+              classNamePrefix="react-select"
+            />
+            <Select
+              placeholder="ค้นหา/เลือกการไฟฟ้าสาขา"
+              noOptionsMessage={() => "ไม่พบข้อมูล"}
+              className="react-select-container"
+              classNamePrefix="react-select"
+            />
+            <Select
+              placeholder="ค้นหา/เลือก Feeder"
+              noOptionsMessage={() => "ไม่พบข้อมูล"}
+              className="react-select-container"
+              classNamePrefix="react-select"
+            />
+            <Select
+              placeholder="ค้นหา/เลือกอุปกรณ์"
+              noOptionsMessage={() => "ไม่พบข้อมูล"}
+              className="react-select-container"
+              classNamePrefix="react-select"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
