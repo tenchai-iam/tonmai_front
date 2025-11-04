e5809e849a8a46567603604527022d67fdcdfe0e
tenchai-iam <tenchai@iamthongtong.me>
2025-11-04 09:15:43 +0700

    Update: Enable protected route

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ export default function App() {
         <Router>
           <div className="App">
             <Routes>
-              <Route path="/" element={<Home />} />
+              {/* <Route path="/" element={<Home />} />
               <Route path="/mapG" element={<MapG />} />
               <Route path="/upgrade" element={<Upgrade />} />
               <Route path="/map" element={<Map />} />
@@ -42,10 +42,10 @@ export default function App() {
               <Route path="/value" element={<Value />} />
               <Route path="/budget" element={<Budget />} />
               <Route path="/data" element={<Data />} />
-              <Route path="/admin" element={<Admin />} />
+              <Route path="/admin" element={<Admin />} /> */}
               
 
-              {/* <Route path="/" element={<ProtectedRoute element={Home} />} />
+              <Route path="/" element={<ProtectedRoute element={Home} />} />
               <Route path="/mapG" element={<ProtectedRoute element={MapG} />} />
               <Route path="/upgrade" element={<ProtectedRoute element={Upgrade} />} />
               <Route
@@ -82,7 +82,7 @@ export default function App() {
                 path="/admin"
                 element={<ProtectedRoute element={Admin} />}
                 allowedLevels={["C"]}
-              /> */}
+              />
 
               <Route path="/callback" element={<Callback />} />
               <Route path="*" element={<Navigate to="/" />} />
