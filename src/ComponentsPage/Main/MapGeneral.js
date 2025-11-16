54ec46d5ac2d7afe2aeb8b63b9ae5025b0f7a10b
tenchai-iam <tenchai@iamthongtong.me>
2025-11-16 10:57:11 +0700

    Update: Revome starting aoj_code state

--- src/ComponentsPage/Main/MapGeneral.js
+++ src/ComponentsPage/Main/MapGeneral.js
@@ -63,7 +63,7 @@ const MapG = () => {
     setSelectedDistrict2(event.target.value);
   };
 
-  const [selectedAoj, setSelectedAoj] = useState("1103101");
+  const [selectedAoj, setSelectedAoj] = useState("");
 
   const [selectedFrequency, setSelectedFrequency] = useState("");
 
@@ -71,7 +71,7 @@ const MapG = () => {
     setSelectedFrequency(event.target.value);
   };
 
-  const [selectedAoj2, setSelectedAoj2] = useState("1103101");
+  const [selectedAoj2, setSelectedAoj2] = useState("");
 
   const [selectedFeeder, setSelectedFeeder] = useState([]);
 
